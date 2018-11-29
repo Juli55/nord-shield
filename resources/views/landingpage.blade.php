bb83214b26b23c5559a7c9098189ad0f1c69e22a
Julian <Julian Bertsch>
2018-11-29 14:56:05 +0100

    init laravel

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="section-header"></div>
+    <div class="section-1"></div>
+    <div class="section-2"></div>
+  </body>
+</html>
