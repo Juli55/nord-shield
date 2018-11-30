32eb96898695ef14b2743f56628478ecca465fb9
Julian <Julian Bertsch>
2018-11-30 17:17:26 +0100

    add jquery, start with first section

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -2,11 +2,20 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Nordshield</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <script src="js/vendor/jquery-3.3.1.min.js" charset="utf-8"></script>
   </head>
   <body>
-    <div class="section-header"></div>
-    <div class="section-1"></div>
-    <div class="section-2"></div>
+    <div id="section-header">
+      <a href="/">Logo</a>
+      <ul>
+        <li><a href="/how">How we protect your privacy</a></li>
+        <li><a href="/about">About us</a></li>
+        <li><a href="/download">Get it for Free</a> </li>
+      </ul>
+    </div>
+    <div id="section-1"></div>
+    <div id="section-2"></div>
   </body>
 </html>
