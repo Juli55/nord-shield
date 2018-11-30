dedb035d31c6c1fd9ee1a5fd860758e2a87ed8c5
Julian <Julian Bertsch>
2018-11-30 16:03:49 +0100

    scaffold html page and add download function

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -5,7 +5,14 @@
     <title></title>
   </head>
   <body>
-    <div class="section-header"></div>
+    <div class="section-header">
+      <a href="/">Logo</a>
+      <ul>
+        <li>How we protect your privacy</li>
+        <li>About us</li>
+        <li> <a href="/download">Get it for Free</a> </li>
+      </ul>
+    </div>
     <div class="section-1"></div>
     <div class="section-3"></div>
   </body>
