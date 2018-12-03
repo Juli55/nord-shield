0804e1ffdc874af88f7e2640a7802867e7c3561e
Julian <Julian Bertsch>
2018-12-03 14:30:50 +0100

    update

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -5,19 +5,45 @@
     <title>Nordshield</title>
     <link rel="stylesheet" href="/css/main.css">
     <script src="js/vendor/jquery-3.3.1.min.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|News+Cycle|Patua+One|Yantramanav" rel="stylesheet">
   </head>
   <body>
     <div id="section-header">
-      <a href="/">Logo</a>
+      <a href="/"><img src="/imgs/logo.png" alt=""></a>
       <ul>
-        <li><a href="/how">How we protect your privacy</a></li>
-        <li><a href="/about">About us</a></li>
-        <li><a href="/download">Get it for Free</a> </li>
+        <li class="sub-page"><a href="/how">How we protect your privacy</a></li>
+        <li class="sub-page"><a href="/about">About us</a></li>
+        <li><a class="button" href="/download"><b>Get it</b> for free</a> </li>
       </ul>
     </div>
-    <div id="section-1"> <p> <b>Enjoy</b> your web experience<br>as if no one is watching you. </p>
-                      <p class="NextSectionDescription">How we protect your privacy</p>
+    <div id="section-1">
+      <p><b>Enjoy</b> your web experience<br>as if no one is watching you.</p>
+      <div><a class="button" href="/download"><b>Get it</b> for free</a></div>
+      <div class="next-section">
+        <p class="NextSectionDescription">How we protect your privacy</p>
+        <i class="NextSectionButton fas fa-chevron-circle-down"></i>
+      </div>
     </div>
-    <div id="section-2"></div>
+    <div id="section-2">
+      <div class="left">
+        <p>We change your registry where possible.</p>
+        <p>We take care of many useful plugins that gradually shield your privacy.</p>
+      </div>
+      <div class="right">
+        <div class="card">
+          <p class="headline"><b>These</b> should be your secrets.</p>
+          <p style="margin-top: 36px">Increase your privacy while keeping your personal user experience at 100%. Automatically.</p>
+          <a style="margin-top: 112px;" class="button" href="/download"><b>Get it</b> for free</a>
+        </div>
+      </div>
+    </div>
+    <script type="text/javascript">
+    $(".NextSectionButton").click(function() {
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $("#section-2").offset().top
+      }, 2000);
+    });
+    </script>
   </body>
 </html>
